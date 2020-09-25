b082eb25902d98f8536f3c7aa0bab09cf9617809
kienan-leung <leung.kienan@gmail.com>
2020-09-25 13:55:05 +1200

    automated board construction

--- minesweeper.js
+++ minesweeper.js
@@ -3,88 +3,103 @@ document.addEventListener("click", checkForWin);
 document.addEventListener("contextmenu", checkForWin);
 
 // Define your `board` object here!
+var board = { cells: [] };
+let boardSize = 4;
+let minePercent = 0.8;
 
-let board = {
-  cells: [
-    {
-      row: 0,
-      col: 0,
-      isMine: true,
-      hidden: true,
-      isMarked: false,
-    },
-    {
-      row: 0,
-      col: 1,
-      isMine: false,
-      hidden: true,
-      isMarked: false,
-    },
-    {
-      row: 0,
-      col: 2,
-      isMine: false,
-      hidden: true,
-      isMarked: false,
-    },
-
-    {
-      row: 1,
-      col: 0,
-      isMine: false,
-      hidden: true,
-      isMarked: false,
-    },
-    {
-      row: 1,
-      col: 1,
-      isMine: true,
-      hidden: true,
-      isMarked: false,
-    },
-    {
-      row: 1,
-      col: 2,
-      isMine: false,
-      hidden: true,
-      isMarked: false,
-    },
-
-    {
-      row: 2,
-      col: 0,
-      isMine: false,
-      hidden: true,
-      isMarked: false,
-    },
-    {
-      row: 2,
-      col: 1,
-      isMine: false,
-      hidden: true,
-      isMarked: false,
-    },
-    {
-      row: 2,
-      col: 2,
-      isMine: false,
-      hidden: true,
-      isMarked: false,
-    },
-  ],
-};
-
-// console.log(board.cells)
-
-for (let i = 0; i < board.cells.length; i++) {
-  countSurroundingMines(board.cells[i]);
+function newBoard() {
+  for (let i = 0; i < boardSize; i++) {
+    for (let j = 0; j < boardSize; j++) {
+      board.cells.push({
+        row: i,
+        col: j,
+        isMine: Math.random() >= minePercent,
+        hidden: true,
+        isMarked: false,
+      });
+    }
+  }
 }
 
-// console.log(totalFalseMines)
+// Basic Hard Coded Minesweeper
+// let board = {
+//   cells: [
+//     {
+//       row: 0,
+//       col: 0,
+//       isMine: true,
+//       hidden: true,
+//       isMarked: false,
+//     },
+//     {
+//       row: 0,
+//       col: 1,
+//       isMine: false,
+//       hidden: true,
+//       isMarked: false,
+//     },
+//     {
+//       row: 0,
+//       col: 2,
+//       isMine: false,
+//       hidden: true,
+//       isMarked: false,
+//     },
+
+//     {
+//       row: 1,
+//       col: 0,
+//       isMine: false,
+//       hidden: true,
+//       isMarked: false,
+//     },
+//     {
+//       row: 1,
+//       col: 1,
+//       isMine: true,
+//       hidden: true,
+//       isMarked: false,
+//     },
+//     {
+//       row: 1,
+//       col: 2,
+//       isMine: false,
+//       hidden: true,
+//       isMarked: false,
+//     },
+
+//     {
+//       row: 2,
+//       col: 0,
+//       isMine: false,
+//       hidden: true,
+//       isMarked: false,
+//     },
+//     {
+//       row: 2,
+//       col: 1,
+//       isMine: false,
+//       hidden: true,
+//       isMarked: false,
+//     },
+//     {
+//       row: 2,
+//       col: 2,
+//       isMine: false,
+//       hidden: true,
+//       isMarked: false,
+//     },
+//   ],
+// };
 
 function startGame() {
   // Don't remove this function call: it makes the game work!
+  newBoard();
   lib.initBoard();
+
+  for (let i = 0; i < board.cells.length; i++) {
+    countSurroundingMines(board.cells[i]);
+  }
 }
 
 // Define this function to look for a win condition:
@@ -129,5 +144,3 @@ function countSurroundingMines(cell) {
   }
   cell.surroundingMines = counter;
 }
-
-console.log(board.cells);
